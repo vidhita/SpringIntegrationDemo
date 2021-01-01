1f1446d07291f0b2ea694c2145aba44d7810c930
vidhita <vidhita@outlook.com>
2021-01-01 16:04:21 +0530

    Spring Integration Demo

--- src/main/java/com/vidhita/integration/PrintService.java
+++ src/main/java/com/vidhita/integration/PrintService.java
@@ -1,8 +1,18 @@
 package com.vidhita.integration;
 
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+
 public class PrintService {
 
     public void print(String message){
         System.out.println(message);
     }
+
+    public void print(Message<String> message){
+        System.out.println(message.getPayload());
+        MessageHeaders headers = message.getHeaders();
+        headers.forEach((k,v)->System.out.println(k + ":"+v));
+    }
+
 }
--- src/main/java/com/vidhita/integration/SpringIntegrationDemoApplication.java
+++ src/main/java/com/vidhita/integration/SpringIntegrationDemoApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
+import org.springframework.messaging.support.MessageBuilder;
 
 @SpringBootApplication
 @Configuration
@@ -16,12 +21,28 @@ public class SpringIntegrationDemoApplication implements ApplicationRunner {
 	@Autowired
 	private CustomGateway gateway;
 
+	@Autowired
+	DirectChannel channel;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIntegrationDemoApplication.class, args);
 	}
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
+
+			channel.subscribe(
+				new MessageHandler() {
+					@Override
+					public void handleMessage(Message<?> message) throws MessagingException {
+						new PrintService().print((Message<String>) message);
+					}
+				}
+		);
 		gateway.print("Hello World From Gateway");
+		Message<String> message = MessageBuilder.withPayload("Using Builder Patter with Direct Channel")
+				.setHeader("headerKey1","value1")
+				.build();
+		channel.send(message);
 	}
 }
