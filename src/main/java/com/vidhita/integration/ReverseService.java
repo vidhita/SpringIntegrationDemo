2021fbffd3f27182b5990ba835c28ef8e18576a5
vidhita <vidhita@outlook.com>
2021-01-06 22:16:33 +0530

    Using PublishSubscribeChannel

--- src/main/java/com/vidhita/integration/ReverseService.java
+++ src/main/java/com/vidhita/integration/ReverseService.java
@@ -1,8 +1,15 @@
 package com.vidhita.integration;
 
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+
+import java.util.Locale;
+
 public class ReverseService {
 
-    public void reverse(String message){
-        System.out.println(new StringBuilder(message).reverse().toString());
+    public Message<String> reverse(String message){
+        //System.out.println(new StringBuilder(message).reverse().toString());
+        return MessageBuilder.withPayload(message.toUpperCase(Locale.ROOT)).build();
+
     }
 }
